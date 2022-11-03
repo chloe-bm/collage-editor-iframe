079b31710ced53ffc181b3db549a04f185cae47a
chloe.bm <chloe@scenify.io>
2022-11-03 16:48:53 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { images } from "./data";
+
+function App() {
+  const iframeRef = React.useRef<HTMLIFrameElement>(null);
+
+  React.useEffect(() => {
+    if (iframeRef.current && iframeRef.current.contentWindow) {
+      setTimeout(() => {
+        const imagesPayload = {
+          type: "IMAGES",
+          data: images,
+        };
+        const backgroundsPayload = {
+          type: "BACKGROUNDS",
+          data: images,
+        };
+        iframeRef.current!.contentWindow!.postMessage(
+          JSON.stringify(imagesPayload),
+          "*"
+        );
+        iframeRef.current!.contentWindow!.postMessage(
+          JSON.stringify(backgroundsPayload),
+          "*"
+        );
+      }, 1500);
+    }
+  }, [iframeRef.current]);
+
+  return (
+    <div>
+      <iframe
+        ref={iframeRef}
+        style={{ height: "100vh", width: "100vw" }}
+        src="http://localhost:5173/"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.ts
@@ -0,0 +1,122 @@
+export const images = [
+  {
+    id: 3493777,
+    preview:
+      "https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 1683492,
+    preview:
+      "https://images.pexels.com/photos/1683492/pexels-photo-1683492.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/1683492/pexels-photo-1683492.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 789555,
+    preview:
+      "https://images.pexels.com/photos/789555/pexels-photo-789555.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/789555/pexels-photo-789555.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 2894944,
+    preview:
+      "https://images.pexels.com/photos/2894944/pexels-photo-2894944.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/2894944/pexels-photo-2894944.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 2437286,
+    preview:
+      "https://images.pexels.com/photos/2437286/pexels-photo-2437286.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/2437286/pexels-photo-2437286.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 670741,
+    preview:
+      "https://images.pexels.com/photos/670741/pexels-photo-670741.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/670741/pexels-photo-670741.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 105808,
+    preview:
+      "https://images.pexels.com/photos/105808/pexels-photo-105808.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/105808/pexels-photo-105808.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 694587,
+    preview:
+      "https://images.pexels.com/photos/694587/pexels-photo-694587.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/694587/pexels-photo-694587.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 4275360,
+    preview:
+      "https://images.pexels.com/photos/4275360/pexels-photo-4275360.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/4275360/pexels-photo-4275360.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 7494171,
+    preview:
+      "https://images.pexels.com/photos/7494171/pexels-photo-7494171.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/7494171/pexels-photo-7494171.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 5846615,
+    preview:
+      "https://images.pexels.com/photos/5846615/pexels-photo-5846615.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/5846615/pexels-photo-5846615.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 5840879,
+    preview:
+      "https://images.pexels.com/photos/5840879/pexels-photo-5840879.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/5840879/pexels-photo-5840879.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 5815904,
+    preview:
+      "https://images.pexels.com/photos/5815904/pexels-photo-5815904.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/5815904/pexels-photo-5815904.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 12736377,
+    preview:
+      "https://images.pexels.com/photos/12736377/pexels-photo-12736377.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/12736377/pexels-photo-12736377.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 9246817,
+    preview:
+      "https://images.pexels.com/photos/9246817/pexels-photo-9246817.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/9246817/pexels-photo-9246817.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 5694948,
+    preview:
+      "https://images.pexels.com/photos/5694948/pexels-photo-5694948.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/5694948/pexels-photo-5694948.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 12229836,
+    preview:
+      "https://images.pexels.com/photos/12229836/pexels-photo-12229836.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/12229836/pexels-photo-12229836.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 12569708,
+    preview:
+      "https://images.pexels.com/photos/12569708/pexels-photo-12569708.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/12569708/pexels-photo-12569708.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 12569707,
+    preview:
+      "https://images.pexels.com/photos/12569707/pexels-photo-12569707.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/12569707/pexels-photo-12569707.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: 12573639,
+    preview:
+      "https://images.pexels.com/photos/12573639/pexels-photo-12573639.jpeg?auto=compress&cs=tinysrgb&h=130",
+    src: "https://images.pexels.com/photos/12573639/pexels-photo-12573639.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+];
